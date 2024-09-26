7f78a4e4cdf21fc02a950c319764e3c61a537222
Damião <jose.faustino@hfa.mil.br>
2024-09-26 14:51:14 -0300

    fim das aulas basicas

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,6 +1,13 @@
 <template>
-    <h3>{{ post.title }}</h3>
-    <p>{{ snippet }}</p>
+    <div class="post">
+        <router-link :to="{name: 'details', params: {id: post.id}}">
+            <h3>{{ post.title }}</h3>
+        </router-link>
+        <p>{{ snippet }}</p>
+        <span v-for="tag in post.tags" :key="tag"> <!--Usado para mostrar as tags de uma por uma-->
+            #{{ tag }}
+        </span>
+    </div>
 </template>
 
 <script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,33 +12,17 @@
 </template>
 
 <script>
-import PostList from '@/components/PostList.vue';
-import { ref } from 'vue';
+import PostList from '@/components/PostList.vue'
+import getPosts from '@/composables/getPosts'
 
 export default {
   name: 'HomeView',
   components: { PostList },
   setup() {
-    const posts = ref([])
-    const error = ref(null)
+    const {posts, error, load} = getPosts()
 
-    const load = async () => {
-      try {
-        let data = await fetch('http://localhost:3000/posts')
-        if (!data.ok) {
-          throw Error('no data available')
-        }
-        posts.value = await data.json()
-        
-
-      } catch (err) {
-        error.value = err.message
-        console.log(error.value);
-
-      }
-    }
     load()
-    const showPosts = ref(true)
+
     return { posts, error }
   }
 }
