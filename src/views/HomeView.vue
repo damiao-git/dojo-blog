1329d0153d9ca3f4b8c63b9aabf868c54c4805b7
Damião <jose.faustino@hfa.mil.br>
2024-09-26 13:00:36 -0300

    Fetch API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="home">
     <h1>home</h1>
-    <PostList v-if="showPosts" :posts="posts"/>
-    <button @click="showPosts = !showPosts">togle posts</button>
-    <button @click="posts.pop()">delete a post</button>
+    <div v-if="error">
+      {{ error }}
+    </div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
@@ -13,15 +17,29 @@ import { ref } from 'vue';
 
 export default {
   name: 'HomeView',
-  components: {PostList},
+  components: { PostList },
   setup() {
-    const posts = ref([
-      {title: 'welcome to the blog', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis consequuntur unde ipsam placeat quibusdam cupiditate molestias vitae culpa dolores commodi, accusamus quas laboriosam, sint nam optio laudantium repellendus deserunt mollitia assumenda molestiae harum obcaecati? Totam officiis labore aspernatur dignissimos porro architecto fugiat earum? Autem porro, repudiandae repellendus eius sequi fugiat tempora culpa velit esse vitae debitis voluptates. Provident deleniti sed doloribus laboriosam delectus temporibus, dolorum ratione, iusto iure voluptate officia! Voluptates earum, sequi quidem nostrum magnam ut dolor necessitatibus dolores, ipsa est repellat voluptate tenetur numquam consequatur voluptas. Architecto molestias sint suscipit illum sed adipisci eligendi dolores id! Doloremque, aut!', id: 1},
-      {title: 'top 5 CSS tips', body: 'Lorem ipsum', id: 2},
-    ])
+    const posts = ref([])
+    const error = ref(null)
 
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts')
+        if (!data.ok) {
+          throw Error('no data available')
+        }
+        posts.value = await data.json()
+        
+
+      } catch (err) {
+        error.value = err.message
+        console.log(error.value);
+
+      }
+    }
+    load()
     const showPosts = ref(true)
-    return { posts, showPosts }
+    return { posts, error }
   }
 }
 </script>
